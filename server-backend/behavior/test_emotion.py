e93ad6c75e9ebf1f730079f89f2f8305483c727e
swaroop7034 <swaroopsundar1@gmail.com>
2026-01-19 21:43:38 +0530

    remove files from commit

--- server-backend/behavior/test_emotion.py
+++
@@ -1,9 +0,0 @@
-from emotion import EmotionAnalyzer
-
-ea = EmotionAnalyzer()
-
-text = "I am very happy about exams"
-emotion = ea.get_emotion(text)
-
-print("Input:", text)
-print("Emotion:", emotion)
--- server-backend/behavior/test_sentiment.py
+++
@@ -1,9 +0,0 @@
-from sentiment import SentimentAnalyzer
-
-sa = SentimentAnalyzer()
-
-text = "I am tired of studying for exams"
-sentiment = sa.get_sentiment(text)
-
-print("Input:", text)
-print("Sentiment:", sentiment)
